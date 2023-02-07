bb076fbd2f5bb3c57b2d431bf219b25b35e12a76
Ryanocerus <39174587+Ryanocerus@users.noreply.github.com>
2023-02-07 11:00:43 +1100

    Progress Update
    - Added Turrets
    - Added Store
    You can select and purchase turrets with currency and place them. Standard and Rocket turret complete.

---
+++ Tower Defence/Assets/Scripts/Shop.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public TurretBlueprint standardTurret;
+    public TurretBlueprint rocketTurret;
+    public TurretBlueprint laserTurret;
+
+    BuildManager buildManager;
+
+    void Start ()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+
+    public void SelectStandardTurret ()
+    {
+        Debug.Log("Standard Turret Selected");
+        buildManager.SelectTurretToBuild(standardTurret);
+    }
+
+    public void SelectRocketTurret()
+    {
+        Debug.Log("Rocket Turret Selected");
+        buildManager.SelectTurretToBuild(rocketTurret);
+    }
+
+    public void SelectLaserTurret()
+    {
+        Debug.Log("Laser Turret Selected");
+        buildManager.SelectTurretToBuild(laserTurret);
+    }
+}
